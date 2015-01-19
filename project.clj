12380effe357981ed697a57990e7cd830ac44627
zach powers <zcharlop@rockefeller.edu>
2015-01-19 17:36:40 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -8,5 +8,6 @@
                  [org.openscience.cdk/cdk-bundle "1.5.10"]
                  [gif-clj "1.0.3"]]
   :main ^:skip-aot drawpeptides.core
+  :repositories [["ebi-repo" "http://www.ebi.ac.uk/intact/maven/nexus/content/repositories/ebi-repo"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
