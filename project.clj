11a2d82d49bc5b955da38c799b064b5502e01479
zachcp <zcharlop@rockefeller.du>
2015-09-06 19:45:13 -0400

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.mikera/imagez "0.5.0"]
-                 [org.openscience.cdk/cdk-bundle "1.5.10"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.mikera/imagez "0.8.0"]
+                 [org.openscience.cdk/cdk-bundle "1.5.11"]
                  [gif-clj "1.0.3"]]
   :main ^:skip-aot drawpeptides.core
   :repositories [["ebi-repo" "http://www.ebi.ac.uk/intact/maven/nexus/content/repositories/ebi-repo"]]
