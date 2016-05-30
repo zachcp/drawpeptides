6431f2d418ddf22389432ea1cd0ba48f9a1a0b18
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-05-30 16:00:28 -0400

    Bump deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [net.mikera/imagez "0.8.0"]
-                 [org.openscience.cdk/cdk-bundle "1.5.11"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha3"]
+                 [net.mikera/imagez "0.10.0"]
+                 [org.openscience.cdk/cdk-depict "1.5.13"]
+                 [org.openscience.cdk/cdk-bundle "1.5.13"]
                  [gif-clj "1.0.3"]]
   :main ^:skip-aot drawpeptides.core
-  :repositories [["ebi-repo" "http://www.ebi.ac.uk/intact/maven/nexus/content/repositories/ebi-repo"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
